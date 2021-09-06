0d3bd788fcece3717147d1dcb6d8b4f1fd6c98f0
FrancescoCina <francesco.cina04@gmail.com>
2021-09-06 17:39:34 +0200

    Fatto Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,105 @@
 <template>
-  <header></header>
-</template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="logo">
+          <img src="@/assets/img/dc-logo.png" alt="Logo" />
+        </div>
+        <nav>
+          <ul class="col">
+            <li v-for="link in links" :key="link.text">
+              <a href="link.url">{{ link.text }}</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template> 
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+header {
+  height: 100px;
+}
+
+.logo {
+  width: 200px;
+  height: 100%;
+}
+
+.logo img {
+  max-width: 100%;
+  height: auto;
+}
+
+a:hover {
+  color: #0282f9;
+}
+
+.col {
+  display: flex;
+  justify-content: center;
+}
 </style>
