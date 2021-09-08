a18fe24da5ef61dab79ff973f08063b85d6e6998
FrancescoCina <francesco.cina04@gmail.com>
2021-09-08 15:40:54 +0200

    Creato componente comicCard, creata props per dati sui fumetti e stampato in pagina

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="comics-container">
+    <div v-for="(comic, index) in comics" :key="index" class="comic-card">
+      <img :src="comic.thumb" :alt="comic.series" />
+      <figcaption>{{ comic.series }}</figcaption>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: ["comics"],
+};
+</script>
+
+<style scoped lang="scss">
+.comics-container {
+  display: flex;
+  flex-wrap: wrap;
+  .comic-card {
+    flex-basis: calc(100% / 6);
+    height: 50px;
+  }
+}
+</style>
